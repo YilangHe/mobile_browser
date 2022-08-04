19b1e2d28d2fcaadd079338b0780eaf20168ba17
Noodles <yih022@ucsd.edu>
2022-08-04 15:55:18 -0600

    Set up WebView View Model

--- mobile_browser/mobile_browser/WebViewVM.swift
+++ mobile_browser/mobile_browser/WebViewVM.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import WebKit
+
+class WebViewVM: ObservableObject {
+    let webView: WKWebView
+    let url: URL
+    
+    init() {
+        webView = WKWebView(frame: .zero)
+        url = URL(string: "https://www.google.com")!
+        
+        loadURL()
+    }
+    
+    func loadURL() {
+        webView.load(URLRequest(url: url))
+    }
+}
