c31a9eb783fe127d58b4e0e3e04d0465e703e6e1
Noodles <yih022@ucsd.edu>
2022-08-04 00:30:05 -0600

    initial files

---
+++ mobile_browser/mobile_browser/mobile_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_browserApp.swift
+//  mobile_browser
+//
+//  Created by 何逸朗 on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct mobile_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
